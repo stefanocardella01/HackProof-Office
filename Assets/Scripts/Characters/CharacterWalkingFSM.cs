1f6d9d73c1554869fb3876e8bf2800beec41ffb4
Federico-Bellaera <fede.bell13.fb@gmail.com>
2026-02-11 15:36:17 +0100

    Scena

--- Assets/Scripts/Characters/CharacterWalkingFSM.cs
+++ Assets/Scripts/Characters/CharacterWalkingFSM.cs
@@ -46,6 +46,8 @@ public class NpcBrain : MonoBehaviour
         GameObject target = _movement.CurrentTarget;
         SeatedCharacter seated = target.GetComponent<SeatedCharacter>();
 
+        Debug.Log($"NpcBrain: Starting social check with target {target.name}, SeatedCharacter component found: {seated != null}");
+
         if (seated != null)
         {
             
