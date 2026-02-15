6b5e80a39088ad5317a9287fba99d16aac994bc6
stefanocardella01 <s341749@studenti.polito.it>
2026-02-15 19:49:59 +0100

    aggiunta ui iniziale, ui finale e modificato dialogo iniziale con receptionist

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject infoPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("HackProof-Office");
+    }
+
+    public void OpenInfo()
+    {
+        infoPanel.SetActive(true);
+    }
+
+    public void CloseInfo()
+    {
+        infoPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        // Solo per test in editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
